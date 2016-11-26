dfd01da7b0c55344c9a945a16a8e29cdc357e06c
sanya1 <okay123456@yandex.ru>
2016-11-26 06:02:39 +0300

    temp

--- src/cli/parser.py
+++ src/cli/parser.py
@@ -1,27 +1,13 @@
+import ply.yacc as yacc
+import ply.lex as lex
 
-
-# -----------------------------------------------------------------------------
-# calc.py
-#
-# A simple calculator with variables -- all in one file.
-# -----------------------------------------------------------------------------
-'''
-tokens = (
-    'QUOTE','DOUBLE_QUOTE', 'EQUALS'
-    'PIPE', 'VAR_START', 'NAME'
-    )
-'''
-'''
-'VAR_START', 'EQUALS'
-
-'''
-tokens = ('PIPE', 'NAME', 'EQUALS')
+tokens = ('PIPE', 'NAME', 'EQUALS', 'QUOTE_STR', 'VALUE_STR')
 
 t_EQUALS       = r'='
 t_PIPE         = r'\|'
 t_NAME         = r'[a-zA-Z_][a-zA-Z0-9_]*'
-
-# Ignored characters
+t_VALUE_STR    = r'[^\' =\|]+'
+t_QUOTE_STR    = r'\'[^\']*\''
 t_ignore = " \t"
 
 def t_newline(t):
@@ -31,21 +17,8 @@ def t_newline(t):
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-    
-# Build the lexer
-import ply.lex as lex
-lexer = lex.lex()
 
-# Parsing rules
 precedence = (('left', 'PIPE'),)
-'''
-precedence = (
-    ('left', 'PIPE')
-    ('left','PLUS','MINUS'),
-    ('left','TIMES','DIVIDE'),
-    ('right','UMINUS'),
-    )
-'''
 
 class Pipe:
     def __init__(self, cmd, nxt):
@@ -54,13 +27,7 @@ class Pipe:
     def __str__(self):
         return "(" + str(self.cmd) + " -> " + str(self.nxt) + ")"
 
-class EnvAssigment:
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-    def __str__(self):
-        return "(" + self.name + "=" + self.value + ")"
-    
+  
 class Cmd:
     def __init__(self, name, env, args):
         self.name = name
@@ -72,8 +39,14 @@ class Cmd:
             ss += str(huy) 
         return self.name + " args : " + str(self.args) + " env : " + ss
     
-# dictionary of names
-
+class EnvAssigment:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+    def __str__(self):
+        return "(" + self.name + "=" + self.value + ")"
+    
+    
 def p_pipe(t):
     '''
     cmd : cmd PIPE cmd 
@@ -89,7 +62,7 @@ def p_cmd(t):
     
 def p_args(t):
     '''
-    args : NAME args
+    args : arg_str args
     '''
     t[0] = [t[1]] + (t[2])
 
@@ -114,61 +87,30 @@ def p_env(t):
     
 def p_env_set(t):
     '''
-    envset : NAME EQUALS NAME
+    envset : NAME EQUALS arg_str
     '''
     t[0] = EnvAssigment(t[1], t[3])
-'''
-names = { }
-
-def p_statement_assign(t):
-    'statement : NAME EQUALS expression'
-    names[t[1]] = t[3]
     
-
-
-def p_statement_expr(t):
-    'statement : expression'
-    print(t[1])
-
-def p_expression_binop(t):
-    expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression
-    if t[2] == '+'  : t[0] = t[1] + t[3]
-    elif t[2] == '-': t[0] = t[1] - t[3]
-    elif t[2] == '*': t[0] = t[1] * t[3]
-    elif t[2] == '/': t[0] = t[1] / t[3]
-
-def p_expression_uminus(t):
-    'expression : MINUS expression %prec UMINUS'
-    t[0] = -t[2]
-
-def p_expression_group(t):
-    'expression : LPAREN expression RPAREN'
-    t[0] = t[2]
-
-def p_expression_number(t):
-    'expression : NUMBER'
+def p_quote_str(t):
+    '''
+    arg_str : QUOTE_STR
+    '''
+    t[0] = t[1][1:-1]
+    
+def p_str(t):
+    '''
+    arg_str : VALUE_STR 
+            | NAME 
+    '''
     t[0] = t[1]
 
-def p_expression_name(t):
-    'expression : NAME'
-    try:
-        t[0] = names[t[1]]
-    except LookupError:
-        print("Undefined name '%s'" % t[1])
-        t[0] = 0
-'''
+    
+    
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
 
-import ply.yacc as yacc
 parser = yacc.yacc()
+lexer = lex.lex()
 
-while True:
-    try:
-        s = input('> ')   # Use raw_input on Python 2
-    except EOFError:
-        break
-    print(parser.parse(s))
+def parse(s):
+    return parser.parse(s)
