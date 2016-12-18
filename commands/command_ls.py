024a8c22de7f3d45cb3d97cfc85b36d3d15626a9
sanya1 <okay123456@yandex.ru>
2016-12-19 01:13:53 +0300

    ls with empty args list supported

--- commands/command_ls.py
+++ commands/command_ls.py
@@ -1,5 +1,8 @@
 import os
 def command_ls(shell_status, args):
-    dir = args[0]
+    if len(args) == 0:
+        dir = os.curdir
+    else :
+        dir = args[0]
     for item in os.listdir(dir):
         shell_status.output_stream += item + "\n"
